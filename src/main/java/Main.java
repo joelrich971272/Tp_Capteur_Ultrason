2bfe4c0da3dfbe490b6c890182a8bedc04326c13
Joel <sjbjdi.com>
2024-02-05 15:35:35 +0100

    added and updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
-public class Main {
+import clavier.In;
+
+public class Main extends LiaisonSerie {
     public static void main(String[] args) {
+        Mb1414 mb1414 = new Mb1414() ;
+        System.out.println(mb1414.listerLesPorts().toString());
+        System.out.println("Quel port com ?");
+        In.readString();
+
 
     }
 }
--- src/main/java/Mb1414.java
+++ src/main/java/Mb1414.java
@@ -1,12 +1,28 @@
 import jssc.SerialPortEvent;
 import jssc.SerialPortException;
 
+import java.text.DecimalFormat;
+import java.time.Duration;
+import java.time.Instant;
+
 public class Mb1414 extends LiaisonSerie {
-   double distance ;
-   public void  calcul (SerialPortEvent Event){
-       System.out.println("reception");
+   private static double distance ;
+   DecimalFormat df = new DecimalFormat("0.## cm");
+   Instant tInit ;
+   Instant tFinal ;
+   public void Mb1414 (){
+      listerLesPorts().forEach(unCom-> System.out.println(unCom));
 
    }
+   public void  calcul (SerialPortEvent Event){
+       calcul();
+
+   }
+
+   private void calcul() {
+   }
+
+
    public void deconnexionCapteur() throws SerialPortException {
       serialPort.closePort();
 
@@ -15,11 +31,24 @@ public class Mb1414 extends LiaisonSerie {
 
    @Override
    public void serialEvent(SerialPortEvent event) {
+      byte[] laTram = lireTrame(event.getEventValue());
+      if (laTram.length==8){
+         Instant.now();
+         distance=(laTram[1]-0x30)*100 + (laTram[2]-0x30)*10 + (laTram[3]-0x30)*2.54;
+         System.out.println(df.format(distance));
+         tFinal = Instant.now();
+         calcul(event);
+         Duration.between(tInit,tFinal);
+
+      }
       super.serialEvent(event);
    }
 
-   public void initialisationCapteur() throws SerialPortException {
-      serialPort.addEventListener(serialPortEvent ->);
+
+   public void initialisationCapteur(String initCapteur) throws SerialPortException {
+   super.initCom(initCapteur);
+   super.configurerParametres(57600,8,0,1);
+
 
    }
 
